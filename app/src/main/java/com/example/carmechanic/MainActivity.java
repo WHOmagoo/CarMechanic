8568eb21a9980616577ed96f7bcd1cd99991ba8a
hmcgough <hrmagoo@gmail.com>
2020-01-16 14:00:01 -0800

    Add login page and user landing page

--- app/src/main/java/com/example/carmechanic/MainActivity.java
+++ app/src/main/java/com/example/carmechanic/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.carmechanic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.carmechanic.data.LoginDataSource;
+import com.example.carmechanic.data.LoginRepository;
+import com.example.carmechanic.ui.login.LoginActivity;
+import com.example.carmechanic.ui.login.LoginViewModel;
+import com.example.carmechanic.ui.login.LoginViewModelFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LoginViewModel lvm = ViewModelProviders.of(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
+        LoginRepository lr = LoginRepository.getInstance(new LoginDataSource());
+
         setContentView(R.layout.activity_main);
+
+        if(!lr.isLoggedIn()) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
